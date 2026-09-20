8ad323cf91b4083a2cad0a5ae4f86d8ca0553bd1
Nyamkhuu <enkhbatn@student.douglascollege.ca>
2026-09-20 14:53:52 -0700

    prisma setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { createObserveModule } from '@nestjs/observe';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { PrismaModule } from './prisma/prisma.module.js';
+import { ConfigModule } from '@nestjs/config';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
@@ -14,6 +16,10 @@ export const { ObserveModule, ObserveInstrument } = createObserveModule();
       appSecret: 'YOUR_APP_SECRET',
       serviceId: 'backend',
     }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
